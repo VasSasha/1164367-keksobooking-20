77da9787558b8649a2396503d200ae934ad4e0b1
VasSasha <65274003+VasSasha@users.noreply.github.com>
2020-06-15 22:42:32 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,33 +1,39 @@
 'use strict';
 var getRandomElement = function (min, max) {
-	var x = Math.floor(min + Math.random() * (max - 1);
+	var x = Math.floor(min + Math.random() * (max - 1));
 	return x;
 }
-var getAvatar = function () {
-	for(var i = 1; i <= 8; i++) {
-	var avatar = 'img/avatars/user0' + i + '.png';
-  } return avatar;
-}
-var features = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"]
-var photos =["http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"];
-var InformationForPost = {
-	avatar: avatar,
-	title:'заголовок предложения',
-	address: '650, 320',
-	price:500 'USD',
-	type:'flat',
-	rooms: 3,
-	guests: 3,
-	checkin:'12:00',
-	checkout:'14:00',
-	features: features[i * getRandomElement(0,5)],
-	description:'строка с описанием',
-	photos:photos[i * getRandomElement(0,2)],
-	location: getRandomElement(0,20) + ' ' + getRandomElement(130,630),
+var features = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"];
+var time = ['12:00', '13:00', '14:00'];
+var photos = ["http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"];
+var getArray = function(arr) {
+	var randomNumber = getRandomElement(0, arr.length - 1);
+	var newArr = [];
+ for(var i = 0; i < randomNumber; i++){
+	arr.push(arr[i]);
+ } return newArr;
 }
 var createMassive = function () {
     var advertisments = [];
     for(var i = 0; i < 8; i++){
-       advertisments.push();
+    var InformationForPost = {
+	avatar:'img/avatars/user0' + i + '.png',
+	title:'заголовок предложения',
+	address: '650, 320',
+	price:500 + 'USD',
+	type:'flat',
+	rooms: 3,
+	guests: 3,
+	checkin:time[i * getRandomElement(0,2)],
+	checkout:time[i * getRandomElement(0,2)],
+	features: getArray(features),
+	description:'строка с описанием',
+	photos: getArray(photos),
+	location: {
+		X: getRandomElement(0,20),
+		Y: getRandomElement(130,630),
+	}
+  };
+       advertisments.push (InformationForPost); 
     }
 }

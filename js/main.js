5486b5114375d661db545cdf6a92e52494a040e3
VasSasha <65274003+VasSasha@users.noreply.github.com>
2020-06-15 23:48:28 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,19 +7,23 @@ var features = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditio
 var time = ['12:00', '13:00', '14:00'];
 var photos = ["http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"];
 var getArray = function(arr) {
-	var randomNumber = getRandomElement(1, arr.length - 1);
+	var randomNumber = 0.5 + Math.random() * ((arr.length - 1) - 1);
 	var newArr = [];
  for(var i = 0; i < randomNumber; i++){
 	newArr.push(arr[i]);
  } return newArr;
 }
+var location = {
+		X: ,
+		Y:
+	}
 var createMassive = function () {
     var advertisments = [];
     for(var i = 1; i <= 8; i++){
     var InformationForPost = {
 	avatar:'img/avatars/user0' + i + '.png',
 	title:'заголовок предложения',
-	address: '650, 320',
+	address: location.X + location.Y,
 	price:500 + 'USD',
 	type:'flat',
 	rooms: 3, 
@@ -29,10 +33,6 @@ var createMassive = function () {
 	features: getArray(features),
 	description:'строка с описанием',
 	photos: getArray(photos),
-	location: {
-		X: 
-		Y:
-	}
   };
        advertisments.push (InformationForPost); 
     }

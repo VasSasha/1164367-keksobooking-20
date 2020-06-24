eccde874e313fb833c9ca7aef31b78392abfd1ea
VasSasha <65274003+VasSasha@users.noreply.github.com>
2020-06-25 00:38:09 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -128,7 +128,7 @@ addAdvertismentCard(advertisments[0]);*/
 
 var mapFilters = document.querySelectorAll('.map__filters fieldset');
 var adForm = document.querySelector('.ad-form');
-var mapPin = document.querySelector('.map__pin--main');
+var mapPinMain = document.querySelector('.map__pin--main');
 var guestsSelected = document.querySelector('#capacity');
 var roomsSelected = document.querySelector('#room_number');
 var getFormsBlocked = function (arr) {
@@ -141,11 +141,11 @@ var getFormsUnblocked = function (arr) {
     arr[i].removeAttribute('disabled');
   }
 };
-var mapPinX = 570 - PIN_WIDTH / 2;
-var mapPinY = 375 - PIN_WIDTH / 2;
+var mapPinX = mapPinMain.style.left;
+var mapPinY = mapPinMain.style.top;
 var getPinAddress = function () {
-  mapPin.style.top = mapPinY + 'px';
-  mapPin.style.left = mapPinX + 'px';
+  var y = mapPinX + 'px';
+  var x = mapPinY  + 'px';
 };
 // неактивное состояние страницы
 var renderNonActiveCondition = function () {
@@ -173,15 +173,15 @@ var onPinClick = function (evt) {
 var renderAddress = function () {
   document.querySelector('#address').value = mapPinX + ', ' + mapPinY;
 };
-mapPin.addEventListener('mousedown', renderAddress);
-mapPin.addEventListener('mousedown', onPinClick);
+mapPinMain.addEventListener('mousedown', renderAddress);
+mapPinMain.addEventListener('mousedown', onPinClick);
 var onEnterPress = function (evt) {
   if ((mapPin === document.activeElement) && (evt.key === 'Enter')) {
     renderActiveCondition();
   }
 };
-mapPin.addEventListener('keydown', onEnterPress);//
-mapPin.addEventListener('keydown', onEnterPress);
+mapPinMain.addEventListener('keydown', onEnterPress);//
+mapPinMain.addEventListener('keydown', onEnterPress);
 
 // валидация форм
 var getValidForm = function () {

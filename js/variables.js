5bd95805d0877246adf2219d40662454563fc1e0
VasSasha <65274003+VasSasha@users.noreply.github.com>
2020-07-20 23:37:25 +0700

    удалила переменную из файла variables

--- js/variables.js
+++ js/variables.js
@@ -5,12 +5,10 @@
   var mapPins = document.querySelector('.map__pins');
   var map = document.querySelector('.map');
   var mainPin = document.querySelector('.map__pin--main');
-  var priceInput = document.querySelector('#price');
 
   window.variables = {
     mapPinsBlock: mapPins,
     map: map,
     mainPin: mainPin,
-    priceInput: priceInput,
   };
 })();
